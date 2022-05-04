9e982cd3cec9a5976f466d76abe1db5f2edb2a57
DevelopJKong <ybaram40@gmail.com>
2022-05-04 16:56:05 +0900

    52 - 부트스트랩 (Bootstrap) 완료

---
+++ bootstrap/app.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Timezone
+ */
+
+date_default_timezone_set('Asia/Seoul');
+
+
+/**
+ * Error Handling
+ */
+ini_set('display_errors','Off');
+
+
+/**
+ * Database Connection (MySQLi);
+ */
+$GLOBALS['DB_CONNECTION'] = mysqli_connect(
+    'localhost:3306',
+    'root',
+    '1234',
+    'phpblog'
+);
+
+if(!$GLOBALS['DB_CONNECTION']){
+    exit;
+}
+
+register_shutdown_function(function () {
+    if(array_key_exists('DB_CONNECTION',$GLOBALS['DB_CONNECTION'])) {
+        mysqli_close($GLOBALS['DB_CONNECTION']);
+    }
+});
+
+/**
+ * Session
+ */
+
+ini_set('session.gc_maxlifetime',1440);
+session_set_cookie_params(1440);
+
+session_start();
